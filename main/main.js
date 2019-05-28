a2d9ce955bf325266a0008b956d947c098a28a13
GaoMingye <gaomingye2010@163.com>
2019-05-29 00:43:55 +0800

    ac

--- main/main.js
+++ main/main.js
@@ -1,4 +1,35 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(s) {
+    //console.log("Debug Info");
+    //return 'Hello World!';
+    var table1 = ["._.", "...", "._.", "._.", "...", "._.", "._.", "._.", "._.", "._."]
+    var table2 = ['|.|', '..|', '._|', '._|', '|_|', '|_.', '|_.', '..|', '|_|', '|_|']
+    var table3 = ['|_|', '..|', '|_.', '._|', '..|', '._|', '|_|', '..|', '|_|', '..|']
+    
+    var len = s.length
+    var result1 = ""
+    var result2 = ""
+    var result3 = ""
+    for(var i = 0; i < len; i++){
+        result1 += table1[parseInt(s[i])]
+        result1 += ' '
+    }
+    for(var i = 0; i < len; i++){
+        result2 += table2[parseInt(s[i])]
+        result2 += ' '
+    }
+    for(var i = 0; i < len; i++){
+        result3 += table3[parseInt(s[i])]
+        result3 += ' '
+    }
+    
+    var ret = result1.substr(0, result1.length-1)
+    ret += "\n"
+    ret += result2.substr(0, result2.length-1)
+    ret += "\n"
+    ret += result3.substr(0, result3.length-1)
+    ret += "\n"
+    
+    return ret
+    
+    
 };
